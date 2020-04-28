e69f90f47230c1e5c4f7b65502f0d802381168d1
陆炎 <ly_0919@163.com>
2020-04-28 18:20:14 +0800

    影院模块

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/CinemaApplication.java
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/CinemaApplication.java
@@ -3,14 +3,13 @@ package com.stylefeng.guns.rest;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
-@EnableAsync
 @EnableDubboConfiguration
-public class GatewayApplication {
+public class CinemaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+
+        SpringApplication.run(CinemaApplication.class, args);
     }
 }
